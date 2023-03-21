a0bf94c8e659a362ab76e2b994193a5e2616a253
jdubdigital <65375250+jdubdigital@users.noreply.github.com>
2023-03-20 23:00:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,72 +1,86 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "https://realtime-database-615b6-default-rtdb.firebaseio.com/"
+  databaseURL: "https://realtime-database-615b6-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const listItemsInDB = ref(database, "listItems");
+
+function updateDocumentTitleWithCount(count) {
+  document.title = count > 0 ? `(${count}) My App` : "My App";
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const listItemsInDB = ref(database, "listItems")
-
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const itemListEl = document.getElementById("item-list")
-
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    push(listItemsInDB, inputValue)
-  
-    clearInputFieldEl()
-    
-})
+async function updatePwaBadge(count) {
+  if ('setAppBadge' in navigator) {
+    try {
+      await navigator.setAppBadge(count);
+    } catch (error) {
+      console.error('Failed to update PWA badge:', error);
+    }
+  }
+}
 
 onValue(listItemsInDB, function (snapshot) {
   if (snapshot.exists()) {
-    let itemsArray = Object.entries(snapshot.val())
+    let itemsArray = Object.entries(snapshot.val());
 
-  console.log(snapshot.val())
+    updateDocumentTitleWithCount(itemsArray.length);
+    updatePwaBadge(itemsArray.length);
 
-  clearItemListEl()
+    clearItemListEl();
 
-  for (let i = 0; i < itemsArray.length; i++) {
-    let currentItem = itemsArray[i]
-    let currentItemID = currentItem[0]
-    let currentItemValue = currentItem[1]
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
 
-    appendItemToItemListEl(currentItem)
-  }
+      appendItemToItemListEl(currentItem);
+    }
   } else {
-    itemListEl.innerHTML = ""
+    itemListEl.innerHTML = "";
+    updateDocumentTitleWithCount(0);
+    updatePwaBadge(0);
   }
+});
 
-})
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const itemListEl = document.getElementById("item-list");
+
+addButtonEl.addEventListener("click", function() {
+  let inputValue = inputFieldEl.value;
+  
+  push(listItemsInDB, inputValue);
+
+  clearInputFieldEl();
+});
 
 function clearItemListEl() {
-  itemListEl.innerHTML = ""
+  itemListEl.innerHTML = "";
 }
 
 function clearInputFieldEl() {
-  inputFieldEl.value = ""
+  inputFieldEl.value = "";
 }
 
 function appendItemToItemListEl(item) {
-  //itemListEl.innerHTML += `<li>${itemValue}</li>`
-  let itemID = item[0]
-  let itemValue = item[1]
+  let itemID = item[0];
+  let itemValue = item[1];
 
-  let newEl = document.createElement("li")
+  let newEl = document.createElement("li");
 
-  newEl.textContent = itemValue
+  newEl.textContent = itemValue;
 
   newEl.addEventListener("click", function() {
-    let exactLocationOfItemInDB = ref(database, `listItems/${itemID}`)
+    let exactLocationOfItemInDB = ref(database, `listItems/${itemID}`);
 
-    remove(exactLocationOfItemInDB)
-  })
+    remove(exactLocationOfItemInDB);
+  });
 
-  itemListEl.append(newEl)
+  itemListEl.append(newEl);
 }
 
 var input = document.getElementById("input-field");
@@ -76,5 +90,3 @@ input.addEventListener("keyup", function(event) {
     document.getElementById("add-button").click();
   }
 });
-
-
